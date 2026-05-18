8207f51cbc87b16535531a9b30317693f33477e8
kimhyeyeon <kho1039@naver.com>
2026-05-18 17:29:59 +0900

    간단한 회원가입

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,30 @@
-import express from 'express';
+import "dotenv/config";
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("서버가 3000 포트에서 정상적으로 실행 중입니다.");
-})
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("셉쎄요는 당신 유행어 - 하셨쎄요는 나의 유행어");
-})
+app.get("/", (req, res) => {
+  res.send("셉쎄요는 당신 유행어 - 하셨쎄요는 나의 유행어");
+});
+
+app.post("/signup", async (req, res) => {
+  const { nickname, email, password } = req.body;
+  await prisma.user.create({
+    data: {
+      nickname,
+      email,
+      password,
+    },
+  });
+  res.send("정상적으로 회원가입이 완료되었습니다.");
+  console.log("정상적으로 회원가입이 완료되었습니다.");
+});
+
+app.listen(3000, () => {
+  console.log("서버가 3000 포트에서 정상적으로 실행 중입니다.");
+});
